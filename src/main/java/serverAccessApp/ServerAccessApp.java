11f3039a71bea5e87490eb55763ea15ee6e191d5
Oleg Atamankin <aov013@mail.ru>
2019-05-18 20:09:20 +0300

    HomeWork8(Task#5)_

--- src/main/java/serverAccessApp/ServerAccessApp.java
+++ src/main/java/serverAccessApp/ServerAccessApp.java
@@ -11,16 +11,16 @@ public class ServerAccessApp {
     public static void main(String[] args) {
 
         AccessLogger accessLogger = new AccessLogger(new LinkedList());
-        Employee ivan = new Employee(2343,"Ivan", "Ivanov", "cleaner");
-        Employee Petr = new Employee(4475,"Petr", "Petrov", "coder");
-        Employee Sergey = new Employee(7896,"Sergey", "Sergeev", "Senior cleaner");
-        Employee Olga = new Employee(5696,"Olga", "Smirnova", "manager");
-        Employee Boris = new Employee(9611,"Boris", "Elkin", "Junior cleaner");
-        Employee Nikolay = new Employee(5544,"Nikolay", "Trudnov", "Security");
-        Employee Vadim = new Employee(7210,"Vadim", "Isaev", "Admin");
-        Employee Rober = new Employee(0033,"Rober", "Adams", "Electrician");
-        Employee Irina = new Employee(1199,"Irina", "Borisova", "Senior admin");
-        Employee Marya = new Employee(9513,"Marya", "Komkova", "Director");
+        Employee ivan = new Employee(2343, "Ivan", "Ivanov", "cleaner");
+        Employee Petr = new Employee(4475, "Petr", "Petrov", "coder");
+        Employee Sergey = new Employee(7896, "Sergey", "Sergeev", "Senior cleaner");
+        Employee Olga = new Employee(5696, "Olga", "Smirnova", "manager");
+        Employee Boris = new Employee(9611, "Boris", "Elkin", "Junior cleaner");
+        Employee Nikolay = new Employee(5544, "Nikolay", "Trudnov", "Security");
+        Employee Vadim = new Employee(7210, "Vadim", "Isaev", "Admin");
+        Employee Rober = new Employee(2893, "Rober", "Adams", "Electrician");
+        Employee Irina = new Employee(1199, "Irina", "Borisova", "Senior admin");
+        Employee Marya = new Employee(9513, "Marya", "Komkova", "Director");
 
         LinkedList<Employee> listEmployee = new LinkedList<>();
         listEmployee.add(ivan);
@@ -46,13 +46,11 @@ public class ServerAccessApp {
         accessLogger.logAccess(Marya);
 
 
+        Iterator<Employee> iter = accessLogger.getList().listIterator(accessLogger.getList().size() - 5);
 
-        Iterator<Employee> iter = accessLogger.getList().listIterator(accessLogger.getList().size()-5);
-
-        while (iter.hasNext()){
+        while (iter.hasNext()) {
             System.out.println(iter.next().getName());
         }
-
 
 
     }
